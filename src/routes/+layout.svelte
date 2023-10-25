50b39bdd62533371711b303cbc23de60085b1ca8
James Beadle <jamesbeadle1989@gmail.com>
2023-10-25 11:05:38 +0100

    Add Layout File

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+    import { initJuno } from "@junobuild/core";
+  
+    onMount(
+      async () =>
+        await initJuno({
+          satelliteId: "kqo2t-faaaa-aaaal-ac2ja-cai",
+        })
+    );
+  </script>
+  
+  <slot />
