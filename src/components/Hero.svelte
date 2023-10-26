0cf58a526b2033ca20d1b8e214ec908414006954
James Beadle <jamesbeadle1989@gmail.com>
2023-10-26 12:20:46 +0100

    Update font

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,22 +1,19 @@
-
-<div class="hero bg-cover bg-center relative">
-    <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="relative z-10 flex flex-col justify-center items-center h-full text-white">
-        <img src="logo.png" alt="Logo" class="h-8 w-auto logo">
-      <p class="text-xl mt-4 mb-4">The Future of Community Football</p>
-      <a href="/projects">
-        <button class="custom-button text-white font-bold py-2 px-4 rounded">
-          Discover
-        </button>
-      </a>
-      
-    </div>
+<div class="hero relative">
+  <img src="hero.png" alt="Hero Background" class="absolute inset-0 w-full h-full object-cover" loading="lazy">
+  <div class="absolute inset-0 bg-black opacity-50"></div>
+  <div class="relative z-10 flex flex-col justify-center items-center h-full text-white">
+      <img src="logo.png" alt="Logo" class="h-8 w-auto logo">
+    <p class="text-xl mt-4 mb-4">The Future of Community Football</p>
+    <a href="/projects">
+      <button class="custom-button text-white font-bold py-2 px-4 rounded">
+        Discover
+      </button>
+    </a>
   </div>
-  
-  <style>
-    .hero {
-      background-image: url('hero.png');
-      height: 100vh;
-    }
-  </style>
-  
+</div>
+
+<style>
+.hero {
+  height: 100vh;
+}
+</style>
