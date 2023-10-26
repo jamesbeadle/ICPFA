982b7a3862a70083e39ace9b0a8a6576be9ac3a0
James Beadle <jamesbeadle1989@gmail.com>
2023-10-26 15:30:21 +0100

    Update tile images

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -7,7 +7,7 @@
     {#each projects as project}
     <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-            <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{project.imagePath}" alt="project-img">
+            <img class="lg:h-96 md:h-96 w-full object-cover object-center" src="{project.imagePath}" alt="project-img">
             <div class="p-6">
                 <h2 class="tracking-widest text-xs title-font font-medium text-white mb-1">{project.name}</h2>
                 <h1 class="title-font text-lg font-medium text-white mb-3">{project.description}</h1>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,16 +1,50 @@
-<footer>
-    <div>
-      <p>ICPFA</p>
-    </div>
-  </footer>
-  
-  <style>
-    /* Add your styling here */
-    footer {
-      background-color: #333;
-      color: white;
-      padding: 1em;
-      text-align: center;
-    }
-  </style>
-  
+<style>
+  .footer {
+      background-color: #2c2c2c;
+      padding: 20px 40px; 
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+  }
+
+  .footer-left {
+      display: flex;
+      align-items: center;
+  }
+
+  .footer-left > * {
+      margin-right: 20px; 
+  }
+
+  .footer-right {
+      display: flex;
+      align-items: center;
+  }
+
+  .footer-logo {
+      width: 100px; 
+  }
+
+  .juno-logo {
+      margin-left: 10px; 
+  }
+
+  .icon {
+      width: 24px; 
+      height: 24px;
+      margin-right: 10px; 
+  }
+</style>
+
+<footer class="footer">
+  <div class="footer-left">
+      <a href="/"><img src="logo.png" alt="Logo" class="footer-logo"></a>
+      <a target="_blank" class="icon" href="https://twitter.com/ICP_FA"><img src="twitter.png" alt="Twitter Icon"></a>
+      <a target="_blank" class="icon" href="https://oc.app/community/jpibo-3yaaa-aaaar-au7hq-cai/channel/182526718610465230108426284010821007369/?ref=zv6hh-xaaaa-aaaar-ac35q-cai"><img src="openchat.png" alt="OpenChat Icon"></a>      
+  </div>
+
+  <div class="footer-right">
+      <span class="text-white">Powered by Juno</span>
+      <a target="_blank" class="icon" href="https://juno.build"><img src="juno.png" alt="Juno Logo" class="juno-logo icon"></a>      
+  </div>
+</footer>
