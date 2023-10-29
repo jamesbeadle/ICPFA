53177695eed6a0c12dbf5ff0f0634c09a5990927
James Beadle <jamesbeadle1989@gmail.com>
2023-10-29 07:53:10 +0000

    Remove hero title max width

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,7 +3,7 @@
   <div class="absolute inset-0 bg-black opacity-50"></div>
   <div class="relative z-10 flex flex-col justify-center items-center h-full text-center text-white">
       <img src="logo.png" alt="Logo" class="h-8 w-auto logo mx-auto">
-      <p class="text-xl mt-4 mb-4 leading-normal max-w-xs">The Future of Community Football</p>
+      <p class="text-xl mt-4 mb-4 leading-normal">The Future of Community Football</p>
       <a href="/projects">
         <button class="custom-button text-white font-bold py-2 px-4 rounded">
           Discover
